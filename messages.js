acd17ec732627ae896ef1c91d71c3c37b21071d8
lms651 <lmschmid@bu.edu>
2025-06-30 14:12:24 -0400

    Added functioning edit & delete buttons

--- messages.js
+++ messages.js
@@ -17,13 +17,50 @@ function onSubmit() {
 
 function displayList() {
     const list = document.getElementById('message-list');
-    list.innerHTML = ''; 
+    list.innerHTML = ''; // clear before re-adding
 
-    messageSet.forEach(element => {
+    messageSet.forEach(message  => {
         const listItem = document.createElement('li');
-        listItem.innerText = element;
+        listItem.classList.add("flex", "items-center", "justify-center", "gap-2", "py-1");
+
+        // Input Text
+        const inputText = document.createElement('span');
+        inputText.innerText = message;
+        inputText.classList.add("text-left");
+
+        // Edit button
+        const editBtn = document.createElement('button');
+        editBtn.innerText = 'Edit';
+        editBtn.classList.add("px-2", "py-1", "bg-yellow-200", "rounded", "mr-1", "opacity-40");
+        editBtn.addEventListener('click', () => onEdit(message));
+
+        // Delete button
+        const deleteBtn = document.createElement('button');
+        deleteBtn.innerText = 'X';
+        deleteBtn.classList.add("px-2", "py-1", "bg-red-200", "rounded", "mr-1", "opacity-40");
+        deleteBtn.addEventListener('click', () => onDelete(message));
+
+        // Append to list item
+        listItem.appendChild(inputText);
+        listItem.appendChild(editBtn);
+        listItem.appendChild(deleteBtn);
+
         list.appendChild(listItem);
-    })
+    });
+}
+
+function onEdit(message) {
+    const messageToEdit = message;
+    onDelete(message);
+    displayList();
+    const textBox = document.getElementById('input-text');
+    textBox.value = messageToEdit;
+}
+
+function onDelete(message) {
+    const messageToDelete = message;
+    messageSet.delete(messageToDelete);
+    displayList();
 }
 
 export {
